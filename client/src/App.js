79c9638cc4c65975fd1ab61ec05c9e8bcbae2e28
adan eshete <adaneeshete560@gmail.com>
2023-10-22 06:30:09 -0700

    code

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes,Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
-  <div>
-    learn react
+  <div >
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}>
+        </Route>
+      </Routes>
+    </Router>
+    
   </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,28 @@
-import React from 'react'
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className='container p-3 vh-100'>
-    welcome to ministry of innovation
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-sm bg-secondary navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="logo.png" alt="Avatar Logo" className="rounded-pill" />
+          </a>
+        </div>
+        <div className="mr-2">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="mr-2">
+          <Link to="/about">About</Link>
+        </div>
+        <div className="mr-2">
+          <Link to="/contact">Contact</Link>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
