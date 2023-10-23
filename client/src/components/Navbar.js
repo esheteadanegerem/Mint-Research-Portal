1d1518347ae4315691cb862f4d875ccde1774714
adan eshete <adaneeshete560@gmail.com>
2023-10-23 15:17:32 -0700

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     
         <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{border:' '}}>
-      <a className="navbar-brand" href="#"><img src='' alt='mint logo '></img></a>
+      <a className="navbar-brand" href="#"><img src='../images/logo.js' alt='mint logo '></img ></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
