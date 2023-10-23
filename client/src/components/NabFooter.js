f8ac8cf1123dee6b3c153b482d4ae470a4ea8748
adan eshete <adaneeshete560@gmail.com>
2023-10-23 12:06:07 -0700

    some foorer and header

--- client/src/components/NabFooter.js
+++ client/src/components/NabFooter.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import '../assets/css/Navfooter.css'
+import logo from '../assets/images/logo.png'
 const NabFooter = () => {
   return (
-    <div>
-      the foooter of the the age
-    </div>
+    <footer id='footer'>
+      <div className='row'>
+        <div className='col'>
+          <img src={logo} alt='logo' id='imag'/>
+        </div>
+        <div className='col'>
+          <h4>Send Us message</h4>
+          <form>
+          <div>  <label className='form-control'>full name:   <input type='text'/></label></div>
+          
+          <div>  <label className='form-control'>full name:   <input type='text'/></label></div>
+          <div>  <label className='form-control'>full name:   <input type='text'/></label></div>
+            <button>send </button>
+          </form>
+        </div>
+        <div className='col'>quick links</div>
+        <div className='col'> contacts</div>
+      </div>
+      
+    </footer>
   )
 }
 
