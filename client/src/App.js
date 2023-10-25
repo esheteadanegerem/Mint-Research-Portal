197cfbe1d97bfdc9f282cca987036cd41df813b3
one-two345 <129900924+one-two345@users.noreply.github.com>
2023-10-25 13:18:19 +0300

    gyg

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes,Route, Switch, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from './pages/Home'
-import { BrowserRouter as Router, Routes,Route, Switch, Link } from 'react-router-dom';
+import PostNews from './pages/admin/PostNews'
+import SetAppointmentDate from './pages/admin/SetAppointmentDate'
+import UpdateUserStatus from './pages/admin/UpdateUserStatus'
+
 
 function App() {
   return (
   <div >
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}>
-        </Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/admin/news/add-news' element={<PostNews/>}/>
+        <Route path='/admin/appointments/add-appointment' element={<SetAppointmentDate/>}/>
+        <Route path='/admin/user-status/add-user-status' element={<UpdateUserStatus/>}/>
       </Routes>
     </Router>
-    
   </div>
   );
 }
